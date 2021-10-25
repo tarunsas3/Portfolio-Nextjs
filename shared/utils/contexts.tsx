1bd4597568a5229fbc83ccc03678df019041b001
Tarun Sastry <60286295+tarunsas3@users.noreply.github.com>
2021-10-25 17:10:33 +0530

    Add files via upload

---
+++ shared/utils/contexts.tsx
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+import { Company, PersonalDetails, Project } from './types';
+
+export const PersonalDetailsContext = createContext<PersonalDetails | undefined>(undefined);
+export const ProjectDetailsContext = createContext<Project[] | undefined>(undefined);
+export const CompanyDetailsContext = createContext<Company[] | undefined>(undefined);
+ 
